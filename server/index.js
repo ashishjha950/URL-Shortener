2e6c610cd37eb8f5169f2faaada25cc3d9781ec8
Ashish Kumar Jha <ashishjha97099@gmail.com>
2025-03-11 23:37:01 +0530

    done

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dbConnection from './dbConnection.js'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv';
+import ShortUrl from './routes/ShortUrl.js'
+import User from './routes/User.js'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+dotenv.config()
+
+dbConnection()
+
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/api/ShortUrl',ShortUrl)
+app.use('/api/User',User)
+
+app.listen(process.env.PORT,()=>{
+    console.log('server started at port',process.env.PORT)
+})
