a6876de072480486e93a8c8359fffff742bf5975
Ashish Kumar Jha <ashishjha97099@gmail.com>
2025-03-17 22:44:29 +0530

    uses react-cookie in place of js-ccokie

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,27 +1,38 @@
-import {useEffect} from 'react'
-import { Navigate } from 'react-router-dom'
-import cookies from 'js-cookie'
-import { toast } from 'react-toastify'
-import axios from 'axios'
-import {useGlobalContext} from './GlobalProvider'
+import { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { useGlobalContext } from './GlobalProvider';
 
-const ProtectedRoute = ({children}) => {
-    const {apiUrl} = useGlobalContext()
-
+const ProtectedRoute = ({ children }) => {
+    const { apiUrl } = useGlobalContext();
+    const cookies = new Cookies(); 
+    
     useEffect(() => {
-        axios.get(`${apiUrl}`,{ withCredentials: true })
-        const token = cookies.get('token',{ secure: true})
-        if (!token) {
-            toast.warning('Login to access URLs', { autoClose: 1000 });
-        }
-    }, [])
+        const verifyToken = async () => {
+            try {
+                await axios.get(`${apiUrl}`, { withCredentials: true });
+                const token = cookies.get('token'); 
+                console.log(token);
 
-   if(!token){
-    return <Navigate to='/login' replace/>
+                if (!token) {
+                    toast.warning('Login to access URLs', { autoClose: 1000 });
+                }
+            } catch (error) {
+                console.error('Error verifying token:', error);
+                toast.error('Unauthorized. Redirecting to login.', { autoClose: 1000 });
+            }
+        };
+
+        verifyToken();
+    }, [apiUrl, cookies]);
+
+    if (!cookies.get('token')) {
+        return <Navigate to="/login" replace />;
     }
 
-    return children
-    
-}
+    return children;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
