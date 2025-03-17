5778f9ae6198b781aec5e62900faaff10b50dd9a
Ashish Kumar Jha <ashishjha97099@gmail.com>
2025-03-17 22:22:24 +0530

    cookie checked in protected route

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,17 +2,19 @@ import {useEffect} from 'react'
 import { Navigate } from 'react-router-dom'
 import cookies from 'js-cookie'
 import { toast } from 'react-toastify'
+import axios from 'axios'
+import {useGlobalContext} from './GlobalProvider'
 
 const ProtectedRoute = ({children}) => {
-    // const token = cookies.get('token')
-    const token = cookies.get('token',{ secure: true})
-    console.log(token)
+    const {apiUrl} = useGlobalContext()
 
     useEffect(() => {
+        axios.get(`${apiUrl}`,{ withCredentials: true })
+        const token = cookies.get('token',{ secure: true})
         if (!token) {
             toast.warning('Login to access URLs', { autoClose: 1000 });
         }
-    }, [token])
+    }, [])
 
    if(!token){
     return <Navigate to='/login' replace/>
