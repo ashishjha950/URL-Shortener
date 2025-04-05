63672bf3fca2dec9c6ee9cca8db22cedcd7a2e75
Ashish Kumar Jha <ashishjha97099@gmail.com>
2025-04-05 07:18:00 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -13,11 +13,7 @@ dotenv.config()
 
 dbConnection()
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }));
+app.use(cors());
 
 app.use(cookieParser())
 app.use(express.json())
